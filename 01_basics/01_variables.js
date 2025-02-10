fc40acbf1c62adfb0368fcf89dd43866c7436f72
Zahhad <153800359+Zahhad@users.noreply.github.com>
2025-02-10 15:17:49 +0000

    data types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 14553
+let email = "hzahhad@gmail.com"
+var password = "12345"
+accountCity = "fsd"
+let accountState;
+
+// accountId = 13555 Assignment to constant variable.Not allowed 
+email = "my@gmail.com"
+password = "qwerty"
+accountCity = "lhr"
+
+console.table([accountId, email, password, accountCity, accountState])
+
+/* 
+-> "var" was used in the past but it created a lot of issue in block scope & functional scope.
+-> "const" & "let" are used in modern-day developing.
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use Strict"; // this is used to tell the computer that all the code in this file will be writen as modern js code.
+
+alert("hello") // this will not work in node
+
+let string = "Zahhad"
+let number = 21 // uper limit of what a number could hold in memory is 2^53. For something bigger use "bigint dataType" 
+let bolean = false
+let null = null // "null" is used when there is intentional absence of object value
+let undefined // "undefined" == value is non-existent
+
+
+
+/*
+-> Code Readability >> Efficient Code
+ */
