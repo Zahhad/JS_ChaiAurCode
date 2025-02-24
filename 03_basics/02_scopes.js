e6d32176ff38caa4ea298041bd8d20ad9cad54a0
Zahhad <153800359+Zahhad@users.noreply.github.com>
2025-02-24 16:18:36 +0000

    scope little deeper

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -12,3 +12,46 @@ if(true){
 // console.log(b); // b is not defined
 // 👆bcz it is defined in local scope and we are calling it in the gloable scope
 console.log("this value of c is not meant to show in this scope: ", c); // using var creates scope realated issues
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5)) // this can be accessed here
+
+function addone(num){
+    return num + 1
+}
+
+
+
+addTwo(5) // this will not be accessed here
+const addTwo = function(num){ 
+    return num + 2
+}
