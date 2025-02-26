bf1142a77bdcfd699444a673951ca50029ea286d
Zahhad <153800359+Zahhad@users.noreply.github.com>
2025-02-26 13:45:34 +0000

    Switch Cases

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,60 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        
+        break;
+    case 2:
+        console.log("Feburary");
+        
+        break;
+    case 3:
+        console.log("March");
+        
+        break;
+    case 4:
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("Default Case");
+        
+        break;
+}
+
+//Similar Example for comparing string values
+
+const months = "Feburary"
+switch (months) {
+    case "January":
+        console.log("January");
+        
+        break;
+    case "Feburary":
+        console.log("Feburary");
+        
+        break;
+    case "March":
+        console.log("March");
+        
+        break;
+    case "April":
+        console.log("April");
+        
+        break;
+
+    default:
+        console.log("Default Case");
+        
+        break;
+}
